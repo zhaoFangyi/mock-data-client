6455eb254b946fd0716426b336aa14dd8a176328
bener <2388697970@qq.com>
2020-07-20 10:31:26 +0800

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@ module.exports = {
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'stylus',
-      patterns: []
-    }
+      patterns: [],
+    },
   },
   configureWebpack: {},
-  chainWebpack: (config) => {
+  chainWebpack(config) {
     config.module
       .rule('md')
       .test(/\.md$/)
@@ -20,17 +20,17 @@ module.exports = {
   devServer: {
     overlay: {
       warnings: false,
-      errors: false
+      errors: false,
     },
     proxy: {
       '/api': {
         pathRewrite: {
-          '^/api': ''
+          '^/api': '',
         },
         // target: 'http://47.96.110.34:38080/'
-        target: 'http://192.168.75.214:9999'
+        target: 'http://192.168.75.214:9999',
         // target: 'http://localhost:9999'
       },
-    }
+    },
   },
 }
