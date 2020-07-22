65e199185045e31d40c1bb6727b69ec32010556f
bener <2388697970@qq.com>
2020-07-22 20:45:30 +0800

    fix: sidebar-layout

--- src/views/manual/index.vue
+++ src/views/manual/index.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
 .manual-page
-  .side-menus
-    router-link.menu-item(
-      tag="div", v-for="menu in menus", :key="menu.file",
-      :to="{query: {f: menu.file}}") {{menu.label}}
+  .side-menus-wrap
+    .side-menus
+      router-link.menu-item(
+        tag="div", v-for="menu in menus", :key="menu.file",
+        :to="{query: {f: menu.file}}") {{menu.label}}
   .md-content
     div(id="markdown-container" v-html="markdownHtml")
 </template>
@@ -45,18 +46,21 @@ export default {
 
 <style lang="less" scoped>
 .manual-page {
-  overflow: auto;
   display: flex;
-  height: calc(100vh - 61px);
+}
+.side-menus-wrap {
+  margin: 15px 0;
 }
 .side-menus {
   overflow: auto;
+  position: sticky;
+  top: 76px;
   width: 250px;
-  border-right: 1px solid #eee;
-  padding: 0 15px;
-  margin: 15px 0;
+  height: calc(100vh - 90px);
   line-height: 1.5;
   font-size: 15px;
+  padding: 0 15px;
+  border-right: 1px solid #eee;
   .menu-item {
     margin-bottom: 5px;
     cursor: pointer;
@@ -72,6 +76,5 @@ export default {
 }
 .md-content {
   flex: 1;
-  overflow: auto;
 }
 </style>
